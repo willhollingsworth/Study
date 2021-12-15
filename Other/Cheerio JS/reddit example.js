82793a085e1d30741bd48991d0ce9d59ee8cb5e8
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-16 04:01:37 +1030

    cheerio examples

---
+++ Other/Cheerio JS/reddit example.js
@@ -0,0 +1,27 @@
+// https://www.scrapingbee.com/blog/web-scraping-javascript/
+
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getPostTitles = async () => {
+	try {
+		const { data } = await axios.get(
+			'https://old.reddit.com/r/programming/'
+		);
+		const $ = cheerio.load(data);
+		const postTitles = [];
+
+		$('div > p.title > a').each((_idx, el) => {
+			const postTitle = $(el).text()
+			postTitles.push(postTitle)
+		});
+
+		return postTitles;
+	} catch (error) {
+		throw error;
+	}
+};
+
+getPostTitles()
+.then((postTitles) => console.log(postTitles));
