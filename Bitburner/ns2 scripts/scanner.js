99daf49b6731a1f9e72fa7017ed3e79f0a146c5d
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-21 14:28:10 +1030

    added money total column

--- Bitburner/ns2 scripts/scanner.js
+++ Bitburner/ns2 scripts/scanner.js
@@ -5,20 +5,23 @@ export async function main(ns) {
         hack_secs,
         hack_amount,
         money_percent,
+        money_total,
         hack_money_per_sec;
     let column = 20;
     var headers = () => {
         table([
             'target',
             'hack chance',
-            'hack time',
-            'hack amount',
-            '$ per sec',
-            'remaining money %',
+            'hack secs',
+            'hack $',
+            'h $/s',
+            '$ total',
+            '$ %',
         ]);
     };
 
     var table = (a) => {
+        // displays data in a spreadsheet like table format for easier reading, currently has fixed width
         let string = '',
             pad = 0,
             length = 0;
@@ -39,6 +42,7 @@ export async function main(ns) {
         hack_amount = Math.round(
             ns.hackAnalyze(target) * ns.getServerMoneyAvailable(target)
         );
+        money_total = Math.round(ns.getServerMoneyAvailable(target));
         money_percent = Math.round(
             (ns.getServerMoneyAvailable(target) /
                 ns.getServerMaxMoney(target)) *
@@ -53,6 +57,7 @@ export async function main(ns) {
             hack_secs,
             hack_amount,
             hack_money_per_sec,
+            money_total,
             money_percent,
         ]);
     }
