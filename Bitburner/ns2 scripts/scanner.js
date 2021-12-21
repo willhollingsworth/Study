0caae80fb5b487c992120f0f33a74dcbe41bab86
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-21 13:29:52 +1030

    output changed to a table

--- Bitburner/ns2 scripts/scanner.js
+++ Bitburner/ns2 scripts/scanner.js
@@ -1,11 +1,37 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    var hosts = ns.scan(ns.getHostname()); // build an array of directly connected hosts
-    var hack_chance = 0;
-    var hack_secs = 0;
-    var hack_amount = 0;
-    var money_percent = 0;
-    var hack_money_per_sec = 0;
+    let hosts = ns.scan(ns.getHostname()); // build an array of directly connected hosts
+    let hack_chance = 0,
+        hack_secs,
+        hack_amount,
+        money_percent,
+        hack_money_per_sec;
+    let column = 20;
+    var headers = () => {
+        table([
+            'target',
+            'hack chance',
+            'hack time',
+            'hack amount',
+            '$ per sec',
+            'remaining money %',
+        ]);
+    };
+
+    var table = (a) => {
+        let string = '',
+            pad = 0,
+            length = 0;
+        for (let x of a) {
+            length = (x + '').length; // convert to string
+            pad = column - length;
+            pad = Array(pad + 1).join(' '); // pad ending with spaces
+            string = string.concat(x);
+            string = string.concat(pad);
+        }
+        ns.tprint(string);
+    };
+    headers();
     for (let target of hosts) {
         // loop over each host
         hack_chance = Math.round(ns.hackAnalyzeChance(target) * 100);
@@ -21,19 +47,13 @@ export async function main(ns) {
         hack_money_per_sec = Math.round(
             (hack_amount / hack_secs) * (hack_chance / 100)
         );
-        ns.tprint(
+        table([
             target,
-            ' : ',
             hack_chance,
-            '% success - ',
             hack_secs,
-            ' secs - $',
             hack_amount,
-            ' total gained - $',
             hack_money_per_sec,
-            ' per sec - ',
             money_percent,
-            '% remaining'
-        );
+        ]);
     }
 }
