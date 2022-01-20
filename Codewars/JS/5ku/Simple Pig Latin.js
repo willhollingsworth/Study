0231243caccbd503076fd4aaf2fa935362b6f4d6
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-01-20 12:58:28 +1030

    codewars js

---
+++ Codewars/JS/5ku/Simple Pig Latin.js
@@ -0,0 +1,18 @@
+function pigIt(str) {
+    let output = [],
+        punctuation = ['.', ',', '!', '#', '?'];
+
+    str.split(' ').forEach((c) => {
+        if (!punctuation.includes(c)) {
+            let word = c.split('');
+            output.push(word.slice(1).join('') + word.slice(0, 1) + 'ay');
+        } else {
+            output.push(c);
+        }
+    });
+    return output.join(' ');
+}
+
+console.log(pigIt('Pig latin is cool'), 'igPay atinlay siay oolcay');
+console.log(pigIt('This is my string'), 'hisTay siay ymay tringsay');
+console.log(pigIt('This is my string !'), 'hisTay siay ymay tringsay');
