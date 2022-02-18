625a65910ee5918cd664f3ee757364ae5153bee8
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-02-18 16:05:32 +1030

    codewars 6ku js

---
+++ Codewars/JS/6ku/create phone number.js
@@ -0,0 +1,24 @@
+function createPhoneNumber(n) {
+    return (
+        '(' +
+        n.slice(0, 3).join('') +
+        ')' +
+        ' ' +
+        n.slice(3, 6).join('') +
+        '-' +
+        n.slice(6, 10).join('')
+    );
+}
+
+console.log(
+    createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),
+    '(123) 456-7890'
+);
+console.log(
+    createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+    '(111) 111-1111'
+);
+console.log(
+    createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),
+    '(123) 456-7890'
+);
