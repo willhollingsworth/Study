84b1b02fc32721341bf7955eaeebfa347a4f68c4
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-01-02 19:07:05 +1030

    duplicate count work

---
+++ Codewars/JS/6ku/Counting Duplicates.js
@@ -0,0 +1,14 @@
+function duplicateCount(text) {
+    //...
+}
+
+console.log(duplicateCount(''), 0);
+console.log(duplicateCount('abcde'), 0);
+console.log(duplicateCount('aabbcde'), 2);
+console.log(duplicateCount('aabBcde'), 2, 'should ignore case');
+console.log(duplicateCount('Indivisibility'), 1);
+console.log(
+    duplicateCount('Indivisibilities'),
+    2,
+    'characters may not be adjacent'
+);
