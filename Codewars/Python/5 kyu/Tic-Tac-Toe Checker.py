d0dbc2b2f9e229d01d8bee54166cb7c3043bcdea
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-01-26 17:42:21 +1030

    tictac refactor

--- Codewars/Python/5 kyu/Tic-Tac-Toe Checker.py
+++ Codewars/Python/5 kyu/Tic-Tac-Toe Checker.py
@@ -10,22 +10,23 @@ def checker(spots):
 
 def is_solved(board):
     result = 0
+    board_checks = []
     for x in board: #horizontal checks
-        result = checker(x) if checker(x) else result
+        board_checks.append(x)
     for x in range(3): # vert checks
-        column = [board[0][x],board[1][x],board[2][x]]
-        result = checker(column) if checker(column) else result
-    #diagonal checks    
-    diagonal1 = [board[0][0],board[1][1],board[2][2]]
-    result = checker(diagonal1) if checker(diagonal1) else result
-    diagonal2 = [board[0][2],board[1][1],board[2][0]]
-    result = checker(diagonal2) if checker(diagonal2) else result
-    
-    if result ==0: # unfinished checker
-        for x in board:
-            if 0 in x:
-                return -1
-    return result
+        board_checks.append([board[0][x],board[1][x],board[2][x]])
+    #diag checks
+    board_checks.append([board[0][0],board[1][1],board[2][2]])
+    board_checks.append([board[0][2],board[1][1],board[2][0]])
+    # run all checks
+    for check in board_checks:
+        check_result = checker(check)
+        if check_result:
+            return check_result
+    for x in board: # unfinished check
+        if 0 in x:
+            return -1
+    return 0
 
 
 # not yet finished
