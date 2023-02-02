780901a98f0cc26e94affc27f62b8081fecfaebe
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-02-02 16:10:33 +1030

    py 5ku Codewars

---
+++ Codewars/Python/5 kyu/Snakes and Ladders.py
@@ -0,0 +1,77 @@
+import codewars_test as test
+
+
+class SnakesLadders():
+    def __init__(self):
+        self.position_p1 = 0
+        self.position_p2 = 0
+        self.player_turn = 1
+        self.game_over = False
+        self.snakes_ladders = {
+            2:38,7:14,8:31,15:26,16:6,21:42,28:84,36:44,46:25,49:11,
+            51:67,62:19,64:60,71:91,74:53,78:98,87:94,89:68,92:88,95:75,99:80
+            }
+
+    def play(self, die1, die2):
+        if self.game_over:
+            return 'Game over!'
+        if self.player_turn == 1:
+            self.position_p1 += die1+die2
+            if die1 != die2:
+                self.player_turn = 2
+            if self.position_p1 == 100:
+                self.game_over = True
+                return 'Player 1 Wins!'
+            elif self.position_p1 > 100:
+                self.position_p1 = 100 - (self.position_p1 - 100)
+            if self.position_p1 in self.snakes_ladders:
+                self.position_p1 = self.snakes_ladders[self.position_p1]
+            return 'Player 1 is on square '+str(self.position_p1)
+        else:
+            self.position_p2 += die1+die2
+            if die1 != die2:
+                self.player_turn = 1 
+            if self.position_p2 == 100:
+                self.game_over = True
+                return 'Player 2 Wins!'
+            elif self.position_p2 > 100:
+                self.position_p2 = 100 - (self.position_p2 - 100)
+            if self.position_p2 in self.snakes_ladders:
+                self.position_p2 = self.snakes_ladders[self.position_p2]
+            return 'Player 2 is on square '+str(self.position_p2)
+
+gametest = SnakesLadders()
+print(gametest.play(1, 1))
+print(gametest.play(1, 5))
+print(gametest.play(6, 2))
+print(gametest.play(1, 1))
+print(gametest.play(6, 6))
+print(gametest.play(6, 6))
+print(gametest.play(6, 6))
+print(gametest.play(6, 6))
+print(gametest.play(6, 6))
+print(gametest.play(6, 6))
+print(gametest.play(6, 6))
+print(gametest.play(6, 6))
+
+
+
+# @test.describe('Example Tests')
+
+# def example_tests():
+#     game = SnakesLadders()
+#     @test.it("Should return: 'Player 1 is on square 38'")
+#     def example_test_case():
+#         test.assert_equals(game.play(1, 1), "Player 1 is on square 38")
+
+#     @test.it("Should return: 'Player 1 is on square 44'")
+#     def example_test_case():
+#         test.assert_equals(game.play(1, 5), "Player 1 is on square 44")
+
+#     @test.it("Should return: 'Player 2 is on square 31'")
+#     def example_test_case():
+#         test.assert_equals(game.play(6, 2), "Player 2 is on square 31")
+
+#     @test.it("Should return: 'Player 1 is on square 25'")
+#     def example_test_case():
+#         test.assert_equals(game.play(1, 1), "Player 1 is on square 25")
