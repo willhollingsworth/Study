7d61b71a0ba439c9843033916e0bf2a3fbea1440
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-02-02 18:38:13 +1030

    wip 5ku py

---
+++ Codewars/Python/5 kyu/Wip/Merged String Checker.py
@@ -0,0 +1,34 @@
+def is_merge(s, part1, part2):
+    position = [0,0]
+    for c in s:
+        if c in part1:
+            old_pos = position[0]
+            new_pos = part1.find(c)
+            if old_pos > new_pos:
+                return False
+            position[0] = new_pos
+        elif c in part2:
+            old_pos = position[1]
+            new_pos = part2.find(c)
+            if old_pos > new_pos:
+                return False
+            position[1] = new_pos
+        else:
+            return False    
+    return True
+
+
+    
+# print(is_merge('codewars', 'code', 'wars'))
+# print(is_merge('codewars', 'cdw', 'oears'))
+# print(is_merge('codewars', 'cod', 'wars'))
+print(is_merge("Can we merge it? Yes, we can!", "Cawe erg t? s, we can!", "n meiYe"))
+
+# import codewars_test as test
+# @test.describe('Merged string checker')
+# def desc1():
+#     @test.it('Sample tests')
+#     def it1():
+#         test.assert_equals(is_merge('codewars', 'code', 'wars'), True)
+#         test.assert_equals(is_merge('codewars', 'cdw', 'oears'),True)
+#         test.assert_equals(is_merge('codewars', 'cod', 'wars'),False)
