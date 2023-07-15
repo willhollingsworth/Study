9b6bab74e9474ec70683a0df2a2b22290fbed1dc
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-07-15 12:31:21 +0930

    5ku py

---
+++ Codewars/Python/5 kyu/Moving Zeros To The End2.py
@@ -0,0 +1,13 @@
+def move_zeros(lst):
+    return [v for v in lst if v!=0] + [0]*lst.count(0)
+
+print([0]*3)
+print(move_zeros(
+        [1, 2, 0, 1, 0, 1, 0, 3, 0, 1]),
+        [1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
+print(move_zeros(
+        [9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]),
+        [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+print(move_zeros([0, 0]), [0, 0])
+print(move_zeros([0]), [0])
+print(move_zeros([]), [])
