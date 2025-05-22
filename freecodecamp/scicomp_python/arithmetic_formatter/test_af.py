d3898eff7ebcb50fc37c840bc1215986d06b7da8
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-05-22 17:56:54 +0930

    adding test for af

---
+++ freecodecamp/scicomp_python/arithmetic_formatter/test_af.py
@@ -0,0 +1,48 @@
+from af import arithmetic_arranger
+
+
+# a list of arguments and expected results
+tests =[
+        ([["3801 - 2", "123 + 49"], 
+        ['  3801      123\n-    2    +  49\n------    -----']]),
+
+        ([["1 + 2", "1 - 9380"],
+        ['  1         1\n+ 2    - 9380\n---    ------']]),
+
+        ([["3 + 855", "3801 - 2", "45 + 43", "123 + 49"],
+        ['    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----']]), 
+
+        ([["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"],
+        ['  11      3801      1      123         1\n+  4    - 2999    + 2    +  49    - 9380\n----    ------    ---    -----    ------']]), 
+
+        ([["44 + 815", "909 - 2", "45 + 43", "123 + 49", "888 + 40", "653 + 87"],
+        ['Error: Too many problems.']]), 
+
+        ([["3 / 855", "3801 - 2", "45 + 43", "123 + 49"],
+        ["Error: Operator must be '+' or '-'."]]),
+
+        ([["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"],
+        ['Error: Numbers cannot be more than four digits.']]),
+
+        ([["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"],
+        ['Error: Numbers must only contain digits.']]),
+
+        ([["3 + 855", "988 + 40"], True],
+        ['    3      988\n+ 855    +  40\n-----    -----\n  858     1028']),
+
+        ([["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"], True],
+        ['   32         1      45      123      988\n- 698    - 3801    + 43    +  49    +  40\n-----    ------    ----    -----    -----\n -666     -3800      88      172     1028']),
+
+] 
+
+def test_answer():
+    for test in tests:
+        args = test[0]
+        expected = test[1][0]
+        # if the test doesn't require an answer send the correct argument
+        args = [args] if isinstance(args[1],str) else args
+        results = arithmetic_arranger(*args)
+        assert results == expected, f'Expected :{expected} but got :{results}'
+
+if __name__ == "__main__":
+    test_answer()
