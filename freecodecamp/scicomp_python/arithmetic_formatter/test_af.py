e16528a74d153db943d14fabb711b149f84230a0
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-05-23 22:49:16 +0930

    fixed test for new testing format

--- freecodecamp/scicomp_python/arithmetic_formatter/test_af.py
+++ freecodecamp/scicomp_python/arithmetic_formatter/test_af.py
@@ -3,13 +3,11 @@ import pytest
 from af import arithmetic_arranger
 from testing_data import tests
 
-tests = [tests[0]]
+# tests = [tests[0:2]]
 
 @pytest.mark.parametrize("input, expected",tests)
 def test_string_match(input, expected):
-        input = [input] if isinstance(input[1],str) else input
         results = arithmetic_arranger(*input)
-        expected = expected
         assert results == expected, f'Expected :{ascii(expected)} but got :{ascii(results)}'
 
 if __name__ == "__main__":
