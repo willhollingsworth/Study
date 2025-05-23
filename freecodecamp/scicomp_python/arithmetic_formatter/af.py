e4dd767ffba36ba6dbdc3646ba92363591716d58
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-05-23 22:49:49 +0930

    cleanup and refactoring

--- freecodecamp/scicomp_python/arithmetic_formatter/af.py
+++ freecodecamp/scicomp_python/arithmetic_formatter/af.py
@@ -1,39 +1,43 @@
-from typing import Union
 
 from testing_data import tests
 
+def arithmetic_arranger(problems: list[str], show_answers: bool = False) -> str:
+    lines = [''] * 3    
 
-
-def arithmetic_arranger(problems: str, show_answers=False) -> str:
     if len(problems) > 5:
         return 'Error: Too many problems.'
-    lines = [''] * 4    
 
     for problem in problems:
         items = parse_string(problem)
         if isinstance(items, str):
+            # On error return error string
             return items
         val1, val2, operator = items
         longest_number = max(len(str(val1)),len(str(val2)))
         if longest_number > 4:
             return 'Error: Numbers cannot be more than four digits.'
-        
-        dashes = '-' * (longest_number + 2) 
-        lines[0] += f'{val1: >6}  '
-        lines[1] += f'{operator + " "+ str(val2): >6}  '
-        lines[2] += f'{dashes:>6}  '
+        char_count = longest_number + 2
+        dashes = '-' * (char_count) 
+        lines[0] += f'{val1:>{char_count}}'
+        lines[1] += f'{operator} {val2:>{char_count-2}}'
+        lines[2] += f'{dashes:>{char_count}}'
 
         if show_answers:
+            lines[3] = ''
             if operator == '+':
                 total = val1 + val2
             elif operator == '-':
                 total = val1 - val2
-            lines[3] += f'{total:>6}  '
-
+            lines[3] += f'{total:>{char_count}}'
+        
+        # add spaces between the lines
+        for i in range(len(lines)):
+            lines[i] += ' ' * 4
     output = '\n'.join(lines)
     return output
 
 def parse_string(input: str) -> list | str:   # -> Any:
+    ''' parse the raw string into a list of two numbers and the operator, return a string msg on error'''
     items = input.split(' ')
     output = []
     try:
