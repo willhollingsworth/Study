51eb96cde65249ed971082626096e70d523fc56c
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-05-23 21:12:02 +0930

    cleanup of logic

--- freecodecamp/scicomp_python/arithmetic_formatter/af.py
+++ freecodecamp/scicomp_python/arithmetic_formatter/af.py
@@ -1,65 +1,63 @@
-import time
+from typing import Union
 
 from testing_data import tests
 
 
 
-def arithmetic_arranger(problems: str, show_answers=False):
+def arithmetic_arranger(problems: str, show_answers=False) -> str:
     if len(problems) > 5:
         return 'Error: Too many problems.'
-    line1 = ''
-    line2 = ''
-    line3 = ''
-    line4 = ''
-    output = ''
+    lines = [''] * 4    
 
     for problem in problems:
-        items = problem.split(' ')
-
-        try:
-            num_1 = int(items[0])
-        except:
-            return 'Error: Numbers must only contain digits.'
-        line1 += f'{num_1: >6}  '
-
-        try:
-            num_2 = int(items[2])
-        except:
-            return 'Error: Numbers must only contain digits.'
-        operator = items[1]
-        if operator not in ['+', '-']:
-            return "Error: Operator must be '+' or '-'."
-        line2 += f'{operator + " "+ str(num_2): >6}  '
-
-        longest_number = max(len(items[0]),len(items[2]))
+        items = parse_string(problem)
+        if isinstance(items, str):
+            return items
+        val1, val2, operator = items
+        longest_number = max(len(str(val1)),len(str(val2)))
         if longest_number > 4:
             return 'Error: Numbers cannot be more than four digits.'
-        line = '-' * (longest_number + 2) 
-        line3 += f'{line:>6}  '
+        
+        dashes = '-' * (longest_number + 2) 
+        lines[0] += f'{val1: >6}  '
+        lines[1] += f'{operator + " "+ str(val2): >6}  '
+        lines[2] += f'{dashes:>6}  '
 
         if show_answers:
             if operator == '+':
-                total = num_1 + num_2
+                total = val1 + val2
             elif operator == '-':
-                total = num_1 - num_2
-            line4 += f'{total:>6}  '
+                total = val1 - val2
+            lines[3] += f'{total:>6}  '
 
-    output += line1 + '\n'
-    output += line2 + '\n' 
-    output += line3 + '\n' 
-    if show_answers:
-        output += line4 + '\n' 
+    output = '\n'.join(lines)
+    return output
+
+def parse_string(input: str) -> list | str:   # -> Any:
+    items = input.split(' ')
+    output = []
+    try:
+        output.append(int(items[0]))
+    except ValueError:
+        return 'Error: Numbers must only contain digits.'
+    
+    try:
+        output.append(int(items[2]))
+    except ValueError:
+        return 'Error: Numbers must only contain digits.'
+    
+    operator = items[1]
+    if operator not in ['+', '-']:
+        return "Error: Operator must be '+' or '-'."
+    
+    output.append(operator)
     return output
 
 if __name__ == "__main__":
-    for test in tests[:2]:
-        inputs = test[0]
-        # print(test[0])
-        if isinstance(inputs[0], str): 
-            results = arithmetic_arranger(inputs)
-        else:
-            results = arithmetic_arranger(*inputs)
+    for test in tests[1:2]:
+        args,expected = test
+        results = arithmetic_arranger(*args)
+        print('result')
         print(results)
+        print('expected')
         print(test[1])
-
-
