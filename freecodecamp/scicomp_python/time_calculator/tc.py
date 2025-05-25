f79e1732239d0872d45ce51b056658a91d068daf
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-05-25 18:06:44 +0930

    rebuilt logic without imports as per specs

--- freecodecamp/scicomp_python/time_calculator/tc.py
+++ freecodecamp/scicomp_python/time_calculator/tc.py
@@ -1,26 +1,44 @@
-from datetime import datetime, timedelta
 import testing_data_tc
+def add_time(start: str, duration: str, day: str = '') -> str:
+    output_list = []
+    symbols = ['AM', 'PM']
 
-def add_time(start: str, duration: str):
-    start_time = datetime.strptime(start, '%I:%M %p')
-    duration_time = datetime.strptime(duration, '%H:%M')
-    duration_delta = timedelta(hours=duration_time.hour, minutes=duration_time.minute)
-    end_time = start_time + duration_delta
-    result: str = end_time.strftime('%I:%M %p').lstrip("0")
-    return result
+    start_hour, start_minute = map(int, start.split(' ')[0].split(':'))
+    duration_hour, duration_minute = map(int, duration.split(':'))
+    symbol = symbols.index(start.split(' ')[1])
+
+    end_minute = (start_minute + duration_minute) % 60
+
+    end_hour = start_hour + duration_hour
+    end_hour += (start_minute + duration_minute) // 60
+    end_hour += symbol * 12
+    formatted_hour = end_hour % 12
+    if formatted_hour == 0:
+        formatted_hour = 12
+
+    symbol = 11 < end_hour % 24 < 24
+
+    output_list.append(f'{formatted_hour}:{end_minute:02d} {symbols[symbol]}')
+
+    days = end_hour // 24
+    if days == 1:
+        output_list.append(' (next day)')
+    elif days > 1:
+        output_list.append(f' ({days} days later)')
+    return ''.join(output_list)
 
 
 if __name__ == "__main__":
-    for test in testing_data.tests[:2]:
+    print('Running tests...')
+    for test in testing_data_tc.tests:
         args, expected = test
         result = add_time(*args)
-        print(f'Testing with args: {args}')
-        print(f'Result:   {result!a}')
-        print(f'Expected: {expected!a}')
-        # matched = results == expected
-        # if not matched:
-        #     print(f'Test failed for args: {args}')
-        #     print('Expected:')
-        #     print(ascii(expected))
-        #     print('Got:')
-        #     print(ascii(results))
+        # print(f'Testing with args: {args}')
+        # print(f'Result:   {result!a}')
+        # print(f'Expected: {expected!a}')
+        matched = result == expected
+        if not matched:
+            print(f'Test failed for args: {args}')
+            print(f'Expected: {expected!a}')
+            print(f'Got:      {result!a}')
+
