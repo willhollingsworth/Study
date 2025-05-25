d93fe1c0dc0f726768a841384c999044a51e22b0
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-05-25 16:51:11 +0930

    initial logic for first 2 tests

--- freecodecamp/scicomp_python/time_calculator/tc.py
+++ freecodecamp/scicomp_python/time_calculator/tc.py
@@ -1,15 +1,22 @@
+from datetime import datetime, timedelta
 import testing_data
 
-def add_time(start, duration):
-    return start + duration
+def add_time(start: str, duration: str):
+    start_time = datetime.strptime(start, '%I:%M %p')
+    duration_time = datetime.strptime(duration, '%H:%M')
+    duration_delta = timedelta(hours=duration_time.hour, minutes=duration_time.minute)
+    end_time = start_time + duration_delta
+    result: str = end_time.strftime('%I:%M %p').lstrip("0")
+    return result
 
 
 if __name__ == "__main__":
-    for test in testing_data.tests:
+    for test in testing_data.tests[:2]:
         args, expected = test
+        result = add_time(*args)
         print(f'Testing with args: {args}')
+        print(f'Result:   {result!a}')
         print(f'Expected: {expected!a}')
-        # results = add_time(*args)
         # matched = results == expected
         # if not matched:
         #     print(f'Test failed for args: {args}')
